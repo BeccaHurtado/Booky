9edeb4cbb69d8a30936b695580c17bd6de70d1fc
BeccaHurtado <rebeccah7.rh@gmail.com>
2023-12-08 19:07:10 -0700

    Initial commit.

---
+++ src/main/java/booklog/example/Booky/BookyApplication.java
@@ -0,0 +1,13 @@
+package booklog.example.Booky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/booklog/example/Booky/BookyApplicationTests.java
@@ -0,0 +1,13 @@
+package booklog.example.Booky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
